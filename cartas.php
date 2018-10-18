f432e815f15c0f46fa609c46157ea941207dfaee
marcosarteaga <marcos118118@gmail.com>
2018-10-18 20:27:47 +0200

    cartas

---
+++ cartas.php
@@ -0,0 +1,10 @@
+<?php
+echo "<link rel='stylesheet' type='text/css' href='cartas.css'>";
+echo "<label>";
+    echo "<input type='checkbox'  />";
+    echo "<div class='card'>";
+        echo "<div class='front'>Front</div>";
+        echo "<div class='back'>Back</div>";
+    echo "</div>";
+echo "</label>";
+?>
