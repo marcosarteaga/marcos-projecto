684d3c996bbdb8ebd24487825171965f33fa40ef
marcosarteaga <marcos118118@gmail.com>
2018-10-19 13:58:47 +0200

    carta

---
+++ girarcarta/girarcarta.js
@@ -0,0 +1,7 @@
+function girar{
+	document.getElementById('id1').src="reverso.png";
+	}
+
+function hola{
+	document.getElementById("p1").innerText= "hola";
+}
---
+++ girarcarta/imagen.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript"  defer src="girarcarta.js"></script>
+</head>
+<body>
+	<img id="id1" src="mario.jpeg">
+	<button onclick="girar()">cambiar</button>
+
+	
+
+
+
+</body>
+</html>
+
+
+
+
