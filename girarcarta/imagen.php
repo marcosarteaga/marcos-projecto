8d26e8b4d217a490b8816baf6b87c370d3a70e67
marcosarteaga <marcos118118@gmail.com>
2018-10-23 23:13:36 +0200

    girarfinal

--- girarcarta/imagen.php
+++ girarcarta/imagen.php
@@ -9,7 +9,16 @@
 	
 	<?php
 	echo "
-	<div id='id1' onclick='girar(this.id)' class='card' >
+	<div id='oculta' onclick='girar(this.id)' class='divoculta' >
+    	<div><img src='mario.jpeg'></div>
+    	<div class='back'><img src='reverso.png'></div>
+  	</div>
+	";
+
+
+
+	echo "
+	<div id='id1' onclick='girar(this.id)' class='card1' >
     	<div><img src='mario.jpeg'></div>
     	<div class='back'><img src='reverso.png'></div>
   	</div>
@@ -26,6 +35,69 @@
 
 	echo "
 	<div id='id3' onclick='girar(this.id)' class='card3' >
+    	<div><img src='mario.jpeg'></div>
+    	<div class='back'><img src='reverso.png'></div>
+  	</div>
+	";
+
+	echo "
+	<div id='id4' onclick='girar(this.id)' class='card4' >
+    	<div><img src='mario.jpeg'></div>
+    	<div class='back'><img src='reverso.png'></div>
+  	</div>
+	";
+
+	echo "
+	<div id='id5' onclick='girar(this.id)' class='card5' >
+    	<div><img src='mario.jpeg'></div>
+    	<div class='back'><img src='reverso.png'></div>
+  	</div>
+	";
+
+	echo "
+	<div id='id6' onclick='girar(this.id)' class='card6' >
+    	<div><img src='mario.jpeg'></div>
+    	<div class='back'><img src='reverso.png'></div>
+  	</div>
+	";
+
+	echo "
+	<div id='id7' onclick='girar(this.id)' class='card7' >
+    	<div><img src='mario.jpeg'></div>
+    	<div class='back'><img src='reverso.png'></div>
+  	</div>
+	";
+
+	echo "
+	<div id='id8' onclick='girar(this.id)' class='card8' >
+    	<div><img src='mario.jpeg'></div>
+    	<div class='back'><img src='reverso.png'></div>
+  	</div>
+	";
+
+	echo "
+	<div id='id9' onclick='girar(this.id)' class='card9' >
+    	<div><img src='mario.jpeg'></div>
+    	<div class='back'><img src='reverso.png'></div>
+  	</div>
+	";
+
+	echo "
+	<div id='id10' onclick='girar(this.id)' class='card10' >
+    	<div><img src='mario.jpeg'></div>
+    	<div class='back'><img src='reverso.png'></div>
+  	</div>
+	";
+
+	echo "
+	<div id='id11' onclick='girar(this.id)' class='card11' >
+    	<div><img src='mario.jpeg'></div>
+    	<div class='back'><img src='reverso.png'></div>
+  	</div>
+	";
+
+	echo "
+	<div id='id12' onclick='girar(this.id)' class='card12' >
     	<div><img src='mario.jpeg'></div>
     	<div class='back'><img src='reverso.png'></div>
   	</div>
